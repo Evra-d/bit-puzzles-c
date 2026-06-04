57fedefcc378b849210339861595afdb77df1fad
Evra-d <gohuinzaliroanevrad@gmail.com>
2026-06-04 10:09:07 +0100

    Reduction comments

--- bit_puzzles.c
+++ bit_puzzles.c
@@ -6,21 +6,14 @@
  */
 int is_little_endian() {
     //Let's take an arbitrary value, much easy to manipulate and represent in memory such as 1
-    int x = 1;
-    /*We only need to inspect the first byte of the integer. 
-     * In Big Endian machine , x will be represent such as : 00 00 00 01
+    /* In Big Endian machine , x will be represent such as : 00 00 00 01
      * In little endian machine , x will be represent such as : 01 00 00 00
     */
 
-    /* So let's convert start be a pointer to the first byte of x in memory
-     * so that it will read one byte instead of 4 in a row like a pointer to an int at the moment of dereferencing
-    */
+    int x = 1;
+    // Let's point to the first byte of x
     unsigned char *start = (unsigned char *)&x;
     
-    /* Then, by the preceding demonstration of representations, if the first byte which is represent by start here 
-     * equals to 1 , then we are in a little endian machine so it return 1. 
-     * If not , we are in a Big endian one, then it return 0.
-    */
     return *start;
 }
 
@@ -31,9 +24,9 @@ int is_little_endian() {
  */
 
 int merge_x_y(int x, int y){
-    //Using bit mask operations in order to extract the LSB of x
+    //Get the LSB of x
     x = x & 0xFF;
-    //Using ~0xFF for setting all bits to 1 except the last bytes put on 0. This help to put the LSB to 0 so that it will be easy to add the new value of x
+    //Set the LSB of y to 0
     y = y & ~0xFF;
     
     //We could've alse use x + y
@@ -57,9 +50,8 @@ unsigned replace_byte (unsigned x, int i, unsigned char b){
  * @return 1 if any bit of x equals to 1 and 0 if not. Using restrictions
  */
 int bit_of_x_1(int x){
-    //I will the NOT operator ! which convert any value to its logical inverse. if x = 0 then x is false, !x = 1 then it is true, and !(!x) = !(1) = 0.
-    //if x != 0 then x is true and !x is false so !x = 0, !(!x) = !(0) = 1 
-    return !(!x);
+    int logical_inverse_value_of_x = !x;
+    return !(logical_inverse_value_of_x);
 }
 
 /* Solution for CS:APP Exercice 2.61-B
@@ -79,7 +71,7 @@ int any_bit_of_x_0(int x){
  * @return 1 if any bit of the LSB bits of x equals to 1 and 0 if not. Using restrictions
  */
 int LSB_bit_of_x_1(int x){
-   //In order to isolate the LSB we will use bit mask operation to the value of x then apply the function bit_of_x_1(int)
+   //Get the LSB of x
    x = x & 0xFF;
    return !(!x);
 }
@@ -91,6 +83,7 @@ int LSB_bit_of_x_1(int x){
 int MSB_bit_of_x_0(int x){
     //Extracting the MSB by right shifting
     //And & 0xFF for avoiding the case where right shifting is donne arithmetically and the bit sign is 1
+    
     x = ( x >> ((sizeof(int) << 3) - 8) ) & 0xFF;
     return !(!(~x));
 }
@@ -103,7 +96,7 @@ int int_shifts_are_arithmetic(){
     //Let's take x = -1 which is 111111111111...111 in binary using two complement
     int x = -1;
     
-    //Then we shift x to get rid of all the actual bit. Then we test if the new value of x has any 1 or 0 using the precedding logic of functions any_bit_of_x_0(int)
+    //Then we shift x to get rid of all the actual bit. Then we test if the new value of x has any 0 using the precedding logic of functions any_bit_of_x_0(int)
     x = x >> ( (sizeof(int) << 3) - 1 );
     return !(~x);
 }
